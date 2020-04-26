0032d57c069f0300b30ba01354b849129f98fdaf
Ivan Yeremenko <ivan.yeremenko@km-ware.us>
2020-04-26 19:40:30 +0300

    Minor improvements.

--- SoundCloudTelegramBot/SoundCloudTelegramBot/Common/Extensions/LoggerExtensions.cs
+++ SoundCloudTelegramBot/SoundCloudTelegramBot/Common/Extensions/LoggerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
+using Microsoft.Extensions.Logging;
 using Telegram.Bot.Types;
 
 namespace SoundCloudTelegramBot.Common.Extensions
@@ -7,9 +8,11 @@ namespace SoundCloudTelegramBot.Common.Extensions
     {
         public static void LogTelegramMessage<T>(this ILogger<T> logger, Update update)
         {
+            var hasLastName = string.IsNullOrEmpty(update.Message.From.LastName);
             logger.LogInformation(
                 $"User {update.Message.Chat.Id} " +
-                $"({update.Message.From.Username} aka {update.Message.From.FirstName + update.Message.From.LastName})" +
+                $"({update.Message.From.Username} aka " +
+                $"{update.Message.From.FirstName + (hasLastName ? " " + update.Message.From.LastName : string.Empty)}" +
                 $" send a message \"{update.Message.Text}\" at {update.Message.Date}.");
         }
     }
