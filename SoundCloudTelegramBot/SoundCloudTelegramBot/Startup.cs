e4d3ecd733e3f94ac503acf6f37f5e30407be0eb
Ivan Yeremenko <ivan.yeremenko@km-ware.us>
2020-05-01 15:16:21 +0300

    Minor improvements.

--- SoundCloudTelegramBot/SoundCloudTelegramBot/Startup.cs
+++ SoundCloudTelegramBot/SoundCloudTelegramBot/Startup.cs
@@ -25,42 +25,30 @@ namespace SoundCloudTelegramBot
     public class Startup
     {
         private readonly IConfiguration configurationRoot;
+        private readonly IHostEnvironment environment;
+        private readonly ILogger<Startup> logger;
         private AppConfiguration appConfiguration;
-        private IServiceCollection serviceCollection;
-        public Startup(IConfiguration configurationRoot)
+
+        public Startup(IConfiguration configurationRoot,
+            IHostEnvironment environment,
+            ILogger<Startup> logger)
         {
             this.configurationRoot = configurationRoot;
+            this.environment = environment;
+            this.logger = logger;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            serviceCollection = services;
-            services.AddControllers().AddNewtonsoftJson();
-            //services.AddSingleton<IAppConfiguration>(appConfiguration);
-            services.AddLogging();
-            services.AddSingleton<IBotProvider, BotProvider>();
-            services.AddHostedService<BotInitializerHostedService>();
-            services.AddScoped<ICurrentMessageProvider, CurrentMessageProvider>();
-            services.AddHttpClient();
-            services.AddTelegramCommands();
-            services.AddSoundCloudServices();
-            services.AddCaches();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            if (environment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 appConfiguration = new AppConfiguration(new TelegramSettings(), new SoundCloudSettings());
                 configurationRoot.Bind(appConfiguration);
             }
-            else if (env.IsProduction())
+            else if (environment.IsProduction())
             {
-                var logger = app.ApplicationServices.GetService<ILoggerFactory>().CreateLogger<Startup>();
                 var envDictionary = new Dictionary<string, string>();
                 foreach (var item in Environment.GetEnvironmentVariables())
                 {
@@ -79,7 +67,27 @@ namespace SoundCloudTelegramBot
                     BotToken = envDictionary["BOTTOKEN"]
                 };
                 var appConfig = new AppConfiguration(telegramSettings, soundCloudSettings);
-                serviceCollection.AddSingleton<IAppConfiguration, AppConfiguration>(_ => appConfig);
+                services.AddSingleton<IAppConfiguration>(appConfig);
+            }
+
+            services.AddControllers().AddNewtonsoftJson();
+            //services.AddSingleton<IAppConfiguration>(appConfiguration);
+            services.AddLogging();
+            services.AddSingleton<IBotProvider, BotProvider>();
+            services.AddHostedService<BotInitializerHostedService>();
+            services.AddScoped<ICurrentMessageProvider, CurrentMessageProvider>();
+            services.AddHttpClient();
+            services.AddTelegramCommands();
+            services.AddSoundCloudServices();
+            services.AddCaches();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseMiddleware<ResponseTimeMiddleware>();
