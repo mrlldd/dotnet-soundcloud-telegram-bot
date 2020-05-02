a4cd6ec7b551a6da98499a0b44b9628ec41d1bd3
Ivan Yeremenko <ivan.yeremenko@km-ware.us>
2020-05-02 14:12:07 +0300

    Notifying devs about any handled errors.

---
+++ SoundCloudTelegramBot/SoundCloudTelegramBot/Common/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telegram.Bot.Types;
+
+namespace SoundCloudTelegramBot.Common.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static string ToMessage(this Exception exception, Message telegramMessage)
+            => "Bot has thrown an exception:\n" +
+               $"Exception: {exception.GetType()}\n" +
+               $"Details: {exception.Message}\n" +
+               $"Stack trace: {exception.StackTrace}\n" +
+               $"User: {telegramMessage.From.Id} - {(telegramMessage.From.FirstName + " " + telegramMessage.From.LastName).Trim()}\n" +
+               $"Username: @{telegramMessage.From.Username}";
+    }
+}
--- SoundCloudTelegramBot/SoundCloudTelegramBot/ExceptionFilters/TelegramExceptionHandler.cs
+++ SoundCloudTelegramBot/SoundCloudTelegramBot/ExceptionFilters/TelegramExceptionHandler.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SoundCloudTelegramBot.Common.Extensions;
 using SoundCloudTelegramBot.Common.Services.CurrentMessageProvider;
 using SoundCloudTelegramBot.Common.Telegram;
 
@@ -10,18 +12,26 @@ namespace SoundCloudTelegramBot.ExceptionFilters
 {
     public class TelegramExceptionHandler : IAsyncExceptionFilter
     {
+        private readonly long[] devIds =
+        {
+            330530584 // @mrlldd
+        };
+
         public async Task OnExceptionAsync(ExceptionContext context)
         {
             var provider = context.HttpContext.RequestServices;
-            var messageProvider = provider.GetService<ICurrentMessageProvider>();
+            var telegramMessage = provider.GetService<ICurrentMessageProvider>().Message;
             var bot = provider.GetService<IBotProvider>().Instance;
             var exception = context.Exception;
-            await bot.SendTextMessageAsync(messageProvider.Message.Chat.Id,
-                "Oops, seems like there is an error during your message \"" + messageProvider.Message.Text + "\":\n\"" + exception.Message +
-                "\"\nSorry :( \nProbably developer already knows about that.");
+            var message = exception.ToMessage(telegramMessage);
+            await Task.WhenAll(devIds
+                .Select(x => bot.SendTextMessageAsync(x, message)));
+            await bot.SendTextMessageAsync(telegramMessage.Chat.Id,
+                "Oops, seems like there is an error during your message handling, sorry :(.\n Notified developer about that.");
             context.HttpContext.Response.StatusCode = 200;
             context.ExceptionHandled = true;
-            provider.GetService<ILoggerFactory>().CreateLogger<TelegramExceptionHandler>().LogError(exception, "Handled an error");
+            provider.GetService<ILoggerFactory>().CreateLogger<TelegramExceptionHandler>()
+                .LogError(exception, "Handled an error");
         }
     }
 }
